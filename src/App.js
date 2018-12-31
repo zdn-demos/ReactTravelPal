837da12e271f902687ffcd26dbd4b2981a727a8f
Zach Nusbaum <zach@zdnmail.com>
2018-12-30 16:54:31 -0800

    create component for geolocation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Geolocation } from './components/geolocation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Geolocation />
       </div>
     );
   }
---
+++ src/components/geolocation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export class Geolocation extends React.Component {
+  render() {
+    return <p>Geolocation goes here.</p>;
+  }
+}
