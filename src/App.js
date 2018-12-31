332f8883fb84462a3c2c1b31898dd1ab4a346f22
Zach Nusbaum <zach@zdnmail.com>
2018-12-30 17:03:34 -0800

    App gets geolocation on load

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import './App.css';
 import { Geolocation } from './components/geolocation';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, lng: null };
+    this.handleCoords = this.handleCoords.bind(this);
+  }
+  handleCoords(coords) {
+    this.setState({ lat: coords.latitude, lng: coords.longitude });
+  }
   render() {
     return (
       <div className="App">
-        <Geolocation />
+        <Geolocation onCoords={this.handleCoords} />
       </div>
     );
   }
--- src/components/geolocation.js
+++ src/components/geolocation.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
 export class Geolocation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+  getLocation() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        this.props.onCoords(position.coords);
+        this.setState({ loading: false });
+      },
+      (error) => console.warn(error)
+    );
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
   render() {
-    return <p>Geolocation goes here.</p>;
+    const message = this.state.loading ? 'Loading...' : `Geolocate success.`;
+    return <p>{message}</p>;
   }
 }
